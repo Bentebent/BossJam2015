bc4b4ef7058f8de5ae3579a864bcecfe69b87569
Kim Restad <kim.restad@gmail.com>
2015-04-11 12:47:52 +0200

    Added generated ground at startup.

--- BossJam2015/Assets/Main.cs
+++ BossJam2015/Assets/Main.cs
@@ -3,11 +3,22 @@ using System.Collections;
 
 public class Main : MonoBehaviour 
 {
+	public GameObject groundPrefab;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		int width = 100;
+		int height = 100;
+
+		for (int h = -height / 2 + 1; h < height / 2; ++h)
+		{
+			for (int w = -width / 2 + 1; w < width / 2; ++w)
+			{
+				GameObject ground = Instantiate(groundPrefab);
+				ground.transform.position = new Vector3(w, 0.0f, h);
+			}
+		}
 	}
 	
 	// Update is called once per frame
