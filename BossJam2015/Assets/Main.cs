c42efc688243c47a289a3b239f500611e0645213
Kim Restad <kim.restad@gmail.com>
2015-04-11 22:30:51 +0200

    Spawnpoint, ground is snowy, lava has texture

--- BossJam2015/Assets/Main.cs
+++ BossJam2015/Assets/Main.cs
@@ -3,27 +3,42 @@ using System.Collections;
 
 public class Main : MonoBehaviour 
 {
+	private const int mWorldWidthBlocks = 50;
+	private const int mWorldDepthBlocks = 50;
+	private const int mWorldHeightBlocks = 3;
+
+	private Vector2 mWorldHalfWidth;
+
 	// Use this for initialization
 	void Start () 
 	{
-		int width = 50;
-		int depth = 50;
-		int height = 3;
+		SetupWorld();
+		SetupPlayerSpawns();
 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Keep time.
+	}
+
+	private void SetupWorld()
+	{
 		GameObject groundPrefab = Resources.Load<GameObject>("GroundTile");
 		GameObject worldBottomPrefab = Resources.Load<GameObject>("WorldBottom");
 		Material lavaMat = Resources.Load<Material>("Materials/LavaMaterial");
 		Vector3 groundScale = groundPrefab.transform.localScale;
 
-		for (int d = -depth / 2 - 1; d < depth / 2; ++d)
+		for (int d = -mWorldDepthBlocks / 2; d <= mWorldDepthBlocks / 2; ++d)
 		{
-			for (int w = -width / 2 - 1; w < width / 2; ++w)
+			for (int w = -mWorldWidthBlocks / 2; w <= mWorldWidthBlocks / 2; ++w)
 			{
-				for (int h = 0; h > -height; --h)
+				for (int h = 0; h > -mWorldHeightBlocks; --h)
 				{
 					GameObject ground = Instantiate(groundPrefab);
 					ground.transform.position = new Vector3(w * groundScale.x, h * groundScale.y, d * groundScale.z);
-		
+
 					if (h < -1)
 						ground.GetComponent<Renderer>().material = lavaMat;
 				}
@@ -31,14 +46,27 @@ public class Main : MonoBehaviour
 		}
 
 		GameObject worldBottom = Instantiate(worldBottomPrefab);
-		worldBottom.GetComponent<Renderer>().material.mainTextureScale = new Vector2(width, depth);
-		worldBottom.transform.localScale = new Vector3((width + 1) * groundScale.x * 0.1f, 1.0f, (depth + 1) * groundScale.z * 0.1f);
-		worldBottom.transform.position = new Vector3(-groundScale.x, groundScale.y * (-height) + (groundScale.y * 0.5f), -groundScale.z);
+		worldBottom.GetComponent<Renderer>().material.mainTextureScale = new Vector2(mWorldWidthBlocks, mWorldDepthBlocks);
+		worldBottom.transform.localScale = new Vector3((mWorldWidthBlocks + 1) * groundScale.x * 0.1f, 1.0f, (mWorldDepthBlocks + 1) * groundScale.z * 0.1f);
+		worldBottom.transform.position = new Vector3(0.0f, groundScale.y * (-mWorldHeightBlocks) + (groundScale.y * 0.5f), 0.0f);
+
+		mWorldHalfWidth = new Vector2((mWorldWidthBlocks - 1) * groundScale.x * 0.5f, (mWorldDepthBlocks - 1) * groundScale.z * 0.5f);
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+	private void SetupPlayerSpawns()
 	{
-	
+		GameObject spawnPrefab = Resources.Load<GameObject>("9x9Platform-Dirt");
+
+		for (int i = 0; i < 4; ++i)
+		{
+			int xModifier = i % 2 == 0 ? -1 : 1;
+			int yModifier = i < 2 ? -1 : 1;
+			GameObject spawnPoint = Instantiate(spawnPrefab);
+			spawnPoint.transform.position = new Vector3(mWorldHalfWidth.x * xModifier, 10.0f, mWorldHalfWidth.y * yModifier);
+		}
+	}
+
+	private void SpawnPlayers()
+	{
 	}
 }
