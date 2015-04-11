7217e1de318a28b25c7169126f60400831d48a06
Kim Restad <kim.restad@gmail.com>
2015-04-11 16:20:37 +0200

    Added mine prefab

---
+++ BossJam2015/Assets/MineBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineBehaviour : MonoBehaviour {
+	public Material mWarningMaterial;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//mWarningMaterial = Resources.Load<Material>("Assets/Materials/MineWarningMaterial");
+		gameObject.GetComponent<Renderer>().material = mWarningMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
